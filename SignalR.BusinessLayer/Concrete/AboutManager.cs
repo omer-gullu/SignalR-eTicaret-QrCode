5474adb14039bf87d73f1fabec2ac0ed419f3743
omergullu <omergullu555@gmail.com>
2024-03-30 00:15:44 +0300

    Bussiness katmanında service clasları tamamlandı.Manger sınıfları yazıldı. ilişkiler kuruldu. Dto Katmanı tamamlandı

---
+++ SignalR.BusinessLayer/Concrete/AboutManager.cs
@@ -0,0 +1,46 @@
+﻿using SignalR.BusinessLayer.Abstract;
+using SignalR.DataAccessLayer.Abstract;
+using SignalR.EntityLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalR.BusinessLayer.Concrete
+{
+    public class AboutManager : IAboutService
+    {
+        private readonly IAboutDal _aboutDal;
+
+        public AboutManager(IAboutDal aboutDal)
+        {
+            _aboutDal = aboutDal;
+        }
+
+        public void TAdd(About entity)
+        {
+            
+        }
+
+        public void TDelete(About entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public About TGetByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<About> TGetListAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TUpdate(About entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
