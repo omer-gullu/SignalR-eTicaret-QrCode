f634b93e960cff42a8ff2c707605740083e63046
omergullu <omergullu555@gmail.com>
2024-04-20 00:09:56 +0300

    Auto Mapper yazıldı. About Api Controller sınıfı yazıldı ve test edildi

---
+++ SignalRApi/Controller/BookingController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SignalRApi.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookingController : ControllerBase
+    {
+    }
+}
